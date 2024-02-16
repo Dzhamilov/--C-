5ff18293a82867c8d90ee888607b8ddfd812f837
Akbar Dzhamilov <akbarshoh-95@mail.ru>
2024-02-16 22:16:22 +0300

    решение первой задачи

--- HmW-1/HmW-1/Program.cs
+++ HmW-1/HmW-1/Program.cs
@@ -4,6 +4,10 @@ namespace HmW_1
 {
     class Program
     {
+        /*Написать программу-калькулятор, вычисляющую выражения вида
+        a + b, a - b, a / b, a * b,
+        введенные из командной строки, и выводящую результат выполнения на
+        экран*/
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
